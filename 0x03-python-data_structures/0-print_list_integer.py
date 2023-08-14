e9f3ddb4be39b0a50e413cf2002b5c62faa02ec0
Megz259 <megan.hodsonrsa@gmail.com>
2023-08-14 09:51:03 -0700

    Task 0- checker fail update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,4 +4,4 @@
 def print_list_integer(my_list=[]):
     """Print all integers of a list"""
     for x in range(len(my_list)):
-        print(f"{my_list[x]:d}")
+        print("{:d}".format(my_list[x]))
