46a2c61b7c52bdfb1734059edcd39265b8eed0b2
Megz259 <megan.hodsonrsa@gmail.com>
2024-04-03 12:58:49 -0700

    Task 9

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    answer = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            answer += a ** b / i
+        except Exception:
+            answer = a + b
+            break
+        return answer
