1676908fb67f6627788a78251c09be39812841f5
Megz259 <megan.hodsonrsa@gmail.com>
2024-04-03 12:51:19 -0700

    Various tasks

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@
 def safe_print_division(a, b):
     try:
         division = a/b
-    except(TypeError):
+    except:
         division = None
     finally:
-        print("Inside result:{}".format(division))
+        print("Inside result: {}".format(division))
         return (division)
