d7f1242c53f41cc9b852c311479468b9ef6e3934
Megz259 <megan.hodsonrsa@gmail.com>
2024-04-10 11:16:22 -0700

    Task 2- chnages

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -74,9 +74,24 @@ class Rectangle:
             self.__height = value
 
             def area(self):
-                """Returns the area of a rectangle"""
+                """Returns the area of a rectangle
+                Attributes:
+                    __width: width
+                    __height: height
+
+                    Return:
+                        Answer"""
                 return (self.__width * self.__height)
 
             def perimeter(self):
-                """Returns the perimeter"""
-                return ((self.__width * 2) + (self.__height * 2))
+                """Returns the perimeter
+                Attributes:
+                    __width: width
+                    __height: height
+
+                    Return:
+                        answer or 0"""
+                if self.__width is 0 or self.__height is 0:
+                    return 0
+                else:
+                    return((self.__width * 2) + (self.__height * 2))
