6e3e04bf1ae9bee13577c79d70a6fc2c6980d762
Megz259 <megan.hodsonrsa@gmail.com>
2024-04-15 05:28:59 -0700

    Reverse prior commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,7 +7,9 @@ module with class MyList
 class MyList(list):
     """Implements sorting method for printing"""
 
+    pass
+
     def print_sorted(self):
     """Prints a list in ascending order"""
 
-    print(sorted(self))
+    print(sorted(list(self)))
