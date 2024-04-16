b8fcfb44662d395adafe3c8b2b0fa1f385b3c93e
Megz259 <megan.hodsonrsa@gmail.com>
2024-04-16 12:47:31 -0700

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module to write a class Student
+"""
+
+
+class Student:
+    """creating class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Creating student
+        Args:
+        first_name (str): first name
+        last_name (str): last name
+        age (int): age of student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self, attrs=None):
+            """Dictionary representation
+            Args:
+            attrs (list): attributes
+            """
+            if attrs is None:
+                return self.__dict__
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                if key is attrs:
+                    new_dict[key] = value
+                    return new_dict
+
+                def reload_from_json(self, json):
+                    for key, value in json.items():
+                        setattr(self, key, value)
