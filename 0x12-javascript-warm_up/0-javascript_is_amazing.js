2bc0950fb31e0884f6c3be3b192f1d7092d142ad
Megz259 <megan.hodsonrsa@gmail.com>
2024-06-18 12:40:40 +0200

    Various tasks

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Print given string
+
+const myVar = 'Javascript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//script that prints 3 lines
+
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+	console.log(args[arg]);
+}
